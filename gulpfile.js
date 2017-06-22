ddf96cfd96b5ea77485d2632ac23c10224777b98
Dan O'Connor <dpo3ys@virginia.edu>
2017-06-21 22:53:09 -0700

    Add nodemon support

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,38 @@
 var gulp = require('gulp');
 
 gulp.task('inject', function () {
+    var inject = require('gulp-inject');
     var wiredep = require('wiredep').stream;
-    var options = {
+    var wiredepOptions = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
     };
 
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], { read: false });
+    var injectOptions = {
+        ignorePath: 'public'
+    };
+
     return gulp.src('./src/views/*.html')
-        .pipe(wiredep(options))
+        .pipe(wiredep(wiredepOptions))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['inject'], function () {
+    var nodemon = require('gulp-nodemon');
+    var serveOptions = {
+        script: 'server.js',
+        delayTime: 1,
+        env: {
+            'PORT': 1337
+        },
+        watch: ['*.js', 'src/**/*.js']
+    };
+
+    return nodemon(serveOptions)
+        .on('restart', function (ev) {
+            console.log('restarting...');
+        });
 });
